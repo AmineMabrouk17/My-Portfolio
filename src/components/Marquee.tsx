5a8389765cf1598c968a0fa2204ecdfcd5c554ac
AmineMabrouk17 <4m1nemabr0uk@gmail.com>
2026-09-02 22:07:08 +0100

    style(about): slow down marquee scroll speed

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -31,7 +31,7 @@ export default function Marquee({ rows, className }: MarqueeProps) {
       gsap.to(row, {
         xPercent: -50 * baseSigns[i],
         repeat: -1,
-        duration: 30,
+        duration: 60,
         ease: "none",
       }),
     );
